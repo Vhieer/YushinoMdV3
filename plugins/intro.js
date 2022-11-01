01120d83cdca0541669528ffb4d9f9a7ba71b56f
xyntix <61931770+xyntix@users.noreply.github.com>
2022-11-01 21:55:40 +0700

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -4,17 +4,17 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
 let krtu = `☰⃟⟣⟔ Yᴏᴜʀ Cᴀʀᴅ Iɴᴛʀᴏ ⟓⟢⃟
-┇⃟⟣⟜ *Nama:* 
-┇⃟⟣⟜ *Umur:* 
-┇⃟⟣⟜ *Alamat:*
-┇⃟⟣⟜ *Hobi:*
-┇⃟⟣⟜ *Pasangan:*
+┇⃟⟣⟜ *Nama:Reeya* 
+┇⃟⟣⟜ *Umur: Idk ? * 
+┇⃟⟣⟜ *Alamat: Idk ?*
+┇⃟⟣⟜ *Hobi: Idk ? *
+┇⃟⟣⟜ *Pasangan: Idk  ? *
 ┖┈┈┈╼╸╸╸╴╴╴ ╳
 `
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, krtu, wm,'MENU','.menu', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://facebook.com/sadtime098",
+    mediaUrl: "https://facebook.com/meteora404",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
     title: 'Iɴᴛʀᴏᴄᴀᴅᴜᴛɪᴏɴ',

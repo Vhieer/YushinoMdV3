7bf33819cbef88284fc9b9dcc9753de65850adf3
Vhieer <115420337+Vhieer@users.noreply.github.com>
2022-11-04 08:44:39 +0700

    Update sticker-attp.js

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,14 +1,10 @@
-import { sticker } from '../lib/sticker.js'
-
 let handler = async (m, { conn, text }) => {
     let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-    let stiker = await sticker(null, global.API('xteam', '/ttp', { file: '', text: teks }), global.packname, global.author)
-    if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
-    throw stiker.toString()
+    conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
 }
-handler.help = ['ttp <teks>']
+handler.help = ['attp <teks>']
 handler.tags = ['sticker']
 
-handler.command = /^ttp$/i
+handler.command = /^attp$/i
 
 export default handler
